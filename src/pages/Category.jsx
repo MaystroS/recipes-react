365b28b113076b4b91414ddf957efcb5376c7fb2
Sergei Maystro <maystro.sergei@gmail.com>
2021-05-19 23:25:38 +0300

    feat(category): added go back button

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import {useParams} from 'react-router-dom';
+import {useHistory, useParams} from 'react-router-dom';
 import {getFilteredByCategory} from "../services/http";
-import {LinearProgress} from "@material-ui/core";
+import {Button, LinearProgress} from "@material-ui/core";
 import MealList from "../components/MealList";
 
 function Category() {
+	const {goBack} = useHistory();
 	const {name} = useParams();
 	const [meals, setMeals] = React.useState([]);
 	const [isLoading, setIsLoading] = React.useState(false);
@@ -23,6 +24,14 @@ function Category() {
 
 	return (
 		<>
+			<Button
+				variant='contained'
+				color='primary'
+				style={{marginBottom: 24}}
+				onClick={goBack}
+			>
+				Go Back
+			</Button>
 			{isLoading ?
 				<LinearProgress/> :
 				<MealList meals={meals}/>}
