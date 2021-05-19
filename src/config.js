83277d040994784130754f78320945d19c4cf83e
Sergei Maystro <maystro.sergei@gmail.com>
2021-05-19 22:40:35 +0300

    feat(http): added axios with api endpoints

---
+++ src/config.js
@@ -0,0 +1,2 @@
+export const API_KEY = '1';
+export const API_URL = `https://themealdb.com/api/json/v1/${API_KEY}`;
--- src/services/http.js
+++ src/services/http.js
@@ -1,29 +1,86 @@
 import axios from "axios";
-import {API_URL, API_KEY} from "../config";
+import {API_URL} from "../config";
 
-const http = axios.create({
-	baseURL: API_URL,
-	headers: {
-		'Authorization': API_KEY
-	}
-})
+const http = axios.create({baseURL: API_URL})
 
-export async function getGoods(lang = 'en') {
-	const endpoint = '/shop';
+/**
+ * @typedef Category
+ * @type Object
+ * @property {string} idCategory - "1"
+ * @property {string} strCategory - "Beef"
+ * @property {string} strCategoryDescription - "Beef is the culinary name for ..."
+ * @property {string} strCategoryThumb - "https://www.themealdb.com/images/category/beef.png"
+ */
+
+/**
+ * @typedef Meal
+ * @type Object
+ * @property {string} idMeal -
+ * @property {string} strMeal - "Apam balik"
+ * @property {string} strMealThumb - "https://www.themealdb.com/images/media/meals/adxcbq1619787919.jpg"
+ */
+
+
+/**
+ * Get all categories list
+ * @return {Promise<Category[]>}
+ */
+export async function getAllCategories() {
+	const endpoint = '/categories.php';
 
 	try {
-		const {data} = await http.get(endpoint, {
-			params: {lang, limit: 10}
-		})
+		const {status, data: {categories}} = await http.get(endpoint);
+		console.log('response:', categories)
 
-		console.log('response:', data)
-
-		if (!data.result) {
+		if (status !== 200) {
 			return [];
 		}
 
-		// noinspection JSUnresolvedVariable
-		return data.shop;
+		return categories;
+	} catch (e) {
+		console.log('Error:', e);
+	}
+}
+
+/**
+ * Get recipes filtered by category name
+ * @param name
+ * @return {Promise<Meal[]>}
+ */
+export async function getFilteredByCategory(name) {
+	const endpoint = '/filter.php';
+
+	try {
+		const {status, data: {meals}} = await http.get(endpoint, {params: {c: name}});
+		console.log('response:', meals)
+
+		if (status !== 200) {
+			return [];
+		}
+
+		return meals;
+	} catch (e) {
+		console.log('Error:', e);
+	}
+}
+
+/**
+ * Get recipes filtered by category name
+ * @param id
+ * @return {Promise<Meal[]>}
+ */
+export async function getMealById(id) {
+	const endpoint = '/lookup.php';
+
+	try {
+		const {status, data: {meals}} = await http.get(endpoint, {params: {i: id}});
+		console.log('response:', meals)
+
+		if (status !== 200) {
+			return [];
+		}
+
+		return meals;
 	} catch (e) {
 		console.log('Error:', e);
 	}
